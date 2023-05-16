f4c231549ae12d76b98b5684f7764de335652b3c
Luiz <luizhmartins11@gmail.com>
2023-05-16 09:57:37 -0300

    refatorando classe IndiceMassaCorporal.

--- src/Cap5/composicaoObjetos/domain/IndiceMassaCorporal.java
+++ src/Cap5/composicaoObjetos/domain/IndiceMassaCorporal.java
@@ -5,4 +5,19 @@ public class IndiceMassaCorporal {
     public double peso;
     public double altura;
 
+    public boolean estaComObesidade(){
+        String estaObeso = "";
+        if (this.resultado >= 30){
+            estaObeso = "O paciente esta com obesidade.";
+            System.out.println(estaObeso);
+            return true;
+        } else
+            estaObeso = "O paciente nao esta com obesidade";
+            System.out.println(estaObeso);
+            return false;
+    }
+    public boolean estaAbaixoDoPesoIdeal(){
+        return this.resultado<=18.5;
+    }
+
 }
--- src/Cap5/composicaoObjetos/test/PacienteTest.java
+++ src/Cap5/composicaoObjetos/test/PacienteTest.java
@@ -7,15 +7,12 @@ public class PacienteTest {
     public static void main(String[] args) {
         Paciente paciente = new Paciente();
         paciente.altura = 1.82;
-        paciente.peso = 200;
+        paciente.peso = 75;
 
         IndiceMassaCorporal imc = paciente.calcularMassaIndiceCorporal();
 
-        if (imc.resultado >=30){
-            System.out.printf("Paciente com altura de: %.2f e de peso: %.2f " +
-                    "está com obesidade",paciente.altura, paciente.peso);
-        }
-        System.out.println();
         System.out.printf("IMC: %.2f%n", imc.resultado);
+
+        imc.estaComObesidade();
     }
 }

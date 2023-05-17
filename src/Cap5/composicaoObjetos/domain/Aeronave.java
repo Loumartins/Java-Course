ef8b114dc44b4877ceda29dcf5a0085175a3f78c
Luiz <luizhmartins11@gmail.com>
2023-05-17 09:06:05 -0300

    adicionando métodos a classe Aeronave e criando o objeto na classe AeronaveTest

--- src/Cap5/composicaoObjetos/domain/Aeronave.java
+++ src/Cap5/composicaoObjetos/domain/Aeronave.java
@@ -1,11 +1,31 @@
 package Cap5.composicaoObjetos.domain;
 
 public class Aeronave {
-    public boolean ativo = true;
+    public boolean ativo;
     public int totalAssentos;
     public int assentosReservados;
 
     public int calcularAssentosDisponiveis(){
         return totalAssentos - assentosReservados;
+    }
+
+    public void desativar(){
+        if (assentosReservados > 0){
+
+        } else {
+            this.ativo = false;
+        }
+    }
+    public void ativar(){
+        this.ativo = true;
+    }
+
+    public void reservarAssentos(int numerosAssentos){
+        if (this.ativo == true){
+            this.assentosReservados+= numerosAssentos;
+        } else {
+            //
+            System.out.println("Nao é possível reservar assentos pois a aeronave esta inativa");
+        }
     }
 }
--- src/Cap5/composicaoObjetos/test/AeronaveTest.java
+++ src/Cap5/composicaoObjetos/test/AeronaveTest.java
@@ -6,7 +6,13 @@ public class AeronaveTest {
     public static void main(String[] args) {
         Aeronave aviaoGol = new Aeronave();
         aviaoGol.totalAssentos = 100;
+        aviaoGol.desativar();
 
+        aviaoGol.reservarAssentos(5);
+
+        aviaoGol.ativar();
+
+        aviaoGol.reservarAssentos(5);
         System.out.printf("GOL (%s): %d assentos disponíveis%n", aviaoGol.ativo ? "Ativo" : "Inativo",
                 aviaoGol.calcularAssentosDisponiveis());
     }

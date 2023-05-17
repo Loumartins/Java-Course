a317d47ec0d0adaaceb795b6d69554b11d77b56e
Luiz <luizhmartins11@gmail.com>
2023-05-17 10:39:43 -0300

    criando metodo para classe Produto

--- src/Cap5/composicaoObjetos/domain/Produto.java
+++ src/Cap5/composicaoObjetos/domain/Produto.java
@@ -3,4 +3,8 @@ package Cap5.composicaoObjetos.domain;
 public class Produto {
     public double precoCusto;
     public double precoVenda;
+
+    public void alterarPrecoCusto(double precoCusto){
+       this.precoCusto = precoCusto;
+    }
 }
--- src/Cap5/composicaoObjetos/test/ServicoDePrecificacaoTest.java
+++ src/Cap5/composicaoObjetos/test/ServicoDePrecificacaoTest.java
@@ -6,13 +6,16 @@ import Cap5.composicaoObjetos.domain.ServicoDePrecificacao;
 public class ServicoDePrecificacaoTest {
     public static void main(String[] args) {
         Produto novoProduto = new Produto();
-        novoProduto.precoCusto = 100;
+       // novoProduto.precoCusto = 100;
+        novoProduto.alterarPrecoCusto(80);
 
 
+        //System.out.printf("Preço de venda: %.2f%n", novoProduto.precoVenda);
         ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
 
         servicoDePrecificacao.definirPrecoVenda(novoProduto, 20);
 
+        System.out.printf("Preço de custo: %.2f%n", novoProduto.precoCusto);
         System.out.printf("Preço de venda: %.2f%n", novoProduto.precoVenda);
     }
 }

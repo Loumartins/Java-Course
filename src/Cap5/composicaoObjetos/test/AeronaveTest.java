f1cc4c297f61f1a18befa404e4c59f978b64fb39
Luiz <luizhmartins11@gmail.com>
2023-05-17 09:18:19 -0300

    adicionando novo objeto na classe AeronaveTest

--- src/Cap5/composicaoObjetos/test/AeronaveTest.java
+++ src/Cap5/composicaoObjetos/test/AeronaveTest.java
@@ -6,14 +6,23 @@ public class AeronaveTest {
     public static void main(String[] args) {
         Aeronave aviaoGol = new Aeronave();
         aviaoGol.totalAssentos = 100;
-        aviaoGol.desativar();
-
-        aviaoGol.reservarAssentos(5);
 
         aviaoGol.ativar();
 
         aviaoGol.reservarAssentos(5);
         System.out.printf("GOL (%s): %d assentos disponíveis%n", aviaoGol.ativo ? "Ativo" : "Inativo",
                 aviaoGol.calcularAssentosDisponiveis());
+
+        System.out.println("-------------------------------");
+        Aeronave aviaoLatam = new Aeronave();
+        aviaoLatam.totalAssentos = 120;
+
+        aviaoLatam.ativar();
+
+        aviaoLatam.reservarAssentos(17);
+        System.out.printf("LATAM (%s): %d assentos disponíveis%n", aviaoLatam.ativo ? "Ativo" : "Inativo",
+                aviaoLatam.calcularAssentosDisponiveis());
+
+
     }
 }

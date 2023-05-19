b5b0664a811dd57dc71667c6f99e537b0db58df7
Luiz <luizhmartins11@gmail.com>
2023-05-19 12:17:16 -0300

    introduzindo outros metodos

---
+++ src/Cap5/composicaoObjetos/exercicios/ex4/domain/NumeroUtil.java
@@ -0,0 +1,39 @@
+package Cap5.composicaoObjetos.exercicios.ex4.domain;
+
+public class NumeroUtil {
+    public static String descobrirNumeroInteiro(int n1, int n2){
+        String resposta = "";
+        if (n1 > n2 ){
+            resposta = "N1 é maior que N2";
+           return resposta;
+        } else
+            resposta = "N2 é maior que N1";
+        return resposta;
+    }
+    public static String descobrirNumeroInteiro(int n1, int n2, int n3){
+        String resposta = "";
+        if (n1 > n2 && n1 > n3){
+            resposta = "N1 é o maior numero";
+            return resposta;
+        } else if (n2 > n1 && n2 > n3){
+            resposta = "N2 é maior";
+            return resposta;
+        } else
+            resposta = "N3 é maior";
+            return resposta;
+    }
+    public static double descobrirNumeroFlutuante(double n1, double n2){
+        if (n1 > n2){
+            return n1;
+        } else
+            return n2;
+    }
+    public static double descobrirNumeroFlutuante(double n1, double n2, double n3){
+        if (n1 > n2 && n1 > n3){
+            return n1;
+        } else if (n2 > n1 && n2 > n3) {
+            return n2;
+        } else
+            return n3;
+    }
+}

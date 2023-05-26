6a2e7de2280c30ff100aa2bada90fda9237c6b04
Luiz <luizhmartins11@gmail.com>
2023-05-26 09:47:12 -0300

    criando classes de turma e aluno

---
+++ src/Cap8/TrabalhandoComArrays/aula7/exemplo/Aluno.java
@@ -0,0 +1,4 @@
+package Cap8.TrabalhandoComArrays.aula7.exemplo;
+
+public class Aluno {
+}
---
+++ src/Cap8/TrabalhandoComArrays/aula7/exemplo/Turma.java
@@ -0,0 +1,7 @@
+package Cap8.TrabalhandoComArrays.aula7.exemplo;
+
+public class Turma {
+    public String nomeIdentificacao;
+    public String nomeProfessora;
+     public Aluno[] alunos;
+}

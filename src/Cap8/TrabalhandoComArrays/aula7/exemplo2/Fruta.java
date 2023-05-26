d7dcc3015c50f9248192d4c27afc76e0ec777a58
Luiz <luizhmartins11@gmail.com>
2023-05-26 10:06:38 -0300

    criando a classe mini mercado e fruta

---
+++ src/Cap8/TrabalhandoComArrays/aula7/exemplo2/Fruta.java
@@ -0,0 +1,5 @@
+package Cap8.TrabalhandoComArrays.aula7.exemplo2;
+
+public class Fruta {
+    public String nome;
+}
---
+++ src/Cap8/TrabalhandoComArrays/aula7/exemplo2/MiniMercado.java
@@ -0,0 +1,6 @@
+package Cap8.TrabalhandoComArrays.aula7.exemplo2;
+
+public class MiniMercado {
+    public String nome;
+    public Fruta[] frutas;
+}

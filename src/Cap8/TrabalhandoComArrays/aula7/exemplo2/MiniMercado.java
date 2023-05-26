07113e71272d93b7f4558fa66bd14133078a50c7
Luiz <luizhmartins11@gmail.com>
2023-05-26 10:12:23 -0300

    adicionando metodos a classe mini mercado

--- src/Cap8/TrabalhandoComArrays/aula7/exemplo2/MiniMercado.java
+++ src/Cap8/TrabalhandoComArrays/aula7/exemplo2/MiniMercado.java
@@ -1,6 +1,16 @@
 package Cap8.TrabalhandoComArrays.aula7.exemplo2;
 
+import java.util.Arrays;
+
 public class MiniMercado {
     public String nome;
     public Fruta[] frutas;
+
+    public void adicionarFruta(Fruta fruta){
+        this.frutas = Arrays.copyOf(frutas, frutas.length+1);
+        this.frutas[this.frutas.length -1]= fruta;
+    }
+    public void imprimirFruta(){
+
+    }
 }

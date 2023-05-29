5dd26ad14f52dc701ea4af9158b0e8baabc95f43
Luiz <luizhmartins11@gmail.com>
2023-05-29 11:36:24 -0300

    refatorando metodo obterMes.

--- src/Cap8/TrabalhandoComArrays/exercicios/exc1/domain/Calendario.java
+++ src/Cap8/TrabalhandoComArrays/exercicios/exc1/domain/Calendario.java
@@ -1,21 +1,13 @@
 package Cap8.TrabalhandoComArrays.exercicios.exc1.domain;
 
 public class Calendario {
+
+    static final String NOMES_MESES[] = {"Janeiro", "Fevereiro", "Março", "Abril","Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
     public static String obterNomeMes(int numeroMes){
-        return switch (numeroMes){
-            case 1 -> "Janeiro";
-            case 2 -> "Fevereiro";
-            case 3 -> "Março";
-            case 4 -> "Abril";
-            case 5 -> "Maio";
-            case 6 -> "Junho";
-            case 7 -> "Julho";
-            case 8 -> "Agosto";
-            case 9 -> "Setembro";
-            case 10 -> "Outubro";
-            case 11 -> "Novembro";
-            case 12 -> "Dezembro";
-            default -> null;
-        };
+        if (numeroMes < 1 || numeroMes > 12){
+            return null;
+        }
+        return NOMES_MESES[numeroMes - 1];
     }
 }
---
+++ src/Cap8/TrabalhandoComArrays/exercicios/exc1/test/CalendarioTest.java
@@ -0,0 +1,10 @@
+package Cap8.TrabalhandoComArrays.exercicios.exc1.test;
+
+import Cap8.TrabalhandoComArrays.exercicios.exc1.domain.Calendario;
+
+public class CalendarioTest {
+    public static void main(String[] args) {
+        String mes = Calendario.obterNomeMes(9);
+        System.out.println(mes);
+    }
+}

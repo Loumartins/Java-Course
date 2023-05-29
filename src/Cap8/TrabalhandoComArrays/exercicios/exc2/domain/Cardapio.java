a2853395b8224ffc25bd39a0d35618408c312733
Luiz <luizhmartins11@gmail.com>
2023-05-29 11:49:27 -0300

    implementando a classe Cardapio

---
+++ src/Cap8/TrabalhandoComArrays/exercicios/exc2/domain/Cardapio.java
@@ -0,0 +1,20 @@
+package Cap8.TrabalhandoComArrays.exercicios.exc2.domain;
+
+public class Cardapio {
+    ItemCardapio[] itens;
+
+    public void adicionarItem(ItemCardapio item){
+        // TODO implementar inclusão de item do cardápio
+    }
+
+    public void removerItem(int indice){
+       // TODO implementar exclusao de item do cardapio da posição informada
+    }
+
+    public void imprimirItensCardapio(double precoMinimo, double precoMaximo){
+        /*
+         TODO implementar codigo para imprimir no console os itens do cardapio que
+              estiverem entre o preço minimo e o preço máximo.
+       */
+    }
+}

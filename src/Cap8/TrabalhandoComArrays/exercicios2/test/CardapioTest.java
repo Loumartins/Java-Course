54cb62f78ab7a442facf6ce46ea5b21a5c95d64e
Luiz <luizhmartins11@gmail.com>
2023-05-30 10:05:33 -0300

    implementando classe test

---
+++ src/Cap8/TrabalhandoComArrays/exercicios2/test/CardapioTest.java
@@ -0,0 +1,11 @@
+package Cap8.TrabalhandoComArrays.exercicios2.test;
+
+import Cap8.TrabalhandoComArrays.exercicios2.domain.Cardapio;
+
+public class CardapioTest {
+    public static void main(String[] args) {
+        Cardapio cardapio = new Cardapio();
+
+
+    }
+}

bbc09cba832bc28b9a179378dd867e8073ec964b
Luiz <luizhmartins11@gmail.com>
2023-05-30 10:09:07 -0300

    refatorando classes com modificador public

--- src/Cap8/TrabalhandoComArrays/exercicios2/domain/ItemCardapio.java
+++ src/Cap8/TrabalhandoComArrays/exercicios2/domain/ItemCardapio.java
@@ -1,12 +1,12 @@
 package Cap8.TrabalhandoComArrays.exercicios2.domain;
 
 public class ItemCardapio {
-    String descricao;
-    double preco;
-    boolean possuiPrecoEntre(double precoMinimo, double precoMaximo) {
+    public String descricao;
+    public double preco;
+   public boolean possuiPrecoEntre(double precoMinimo, double precoMaximo) {
         return preco >= precoMinimo && preco <= precoMaximo;
     }
-    void imprimir() {
+   public void imprimir() {
         System.out.printf("%s x R$%.2f%n", descricao, preco);
     }
 

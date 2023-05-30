3faa2398b4af360d669b8b920cae8202b2b611ff
Luiz <luizhmartins11@gmail.com>
2023-05-30 14:32:56 -0300

    estudos iniciais sobre varags

---
+++ src/Cap8/TrabalhandoComArrays/aula10/domain/Fatura.java
@@ -0,0 +1,8 @@
+package Cap8.TrabalhandoComArrays.aula10.domain;
+
+public class Fatura {
+    public int numero;
+
+    public double valorTotal;
+
+}
---
+++ src/Cap8/TrabalhandoComArrays/aula10/domain/ServicoCobranca.java
@@ -0,0 +1,12 @@
+package Cap8.TrabalhandoComArrays.aula10.domain;
+
+public class ServicoCobranca {
+    public void pagar(Fatura fatura,  String[] emailsCobranca){
+        System.out.printf("Fatura %d, no valor total de R$%.2f, foi paga!%n",
+                fatura.numero, fatura.valorTotal);
+
+        for (String email: emailsCobranca){
+            System.out.printf("Fatura %d enviada para %s%n", fatura.numero, email);
+        }
+    }
+}

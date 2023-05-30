080803f8980b31ce40f6f60e2164967a8c913db7
Luiz <luizhmartins11@gmail.com>
2023-05-30 10:09:52 -0300

    realizando test

--- src/Cap8/TrabalhandoComArrays/exercicios2/test/CardapioTest.java
+++ src/Cap8/TrabalhandoComArrays/exercicios2/test/CardapioTest.java
@@ -1,11 +1,20 @@
 package Cap8.TrabalhandoComArrays.exercicios2.test;
 
 import Cap8.TrabalhandoComArrays.exercicios2.domain.Cardapio;
+import Cap8.TrabalhandoComArrays.exercicios2.domain.ItemCardapio;
 
 public class CardapioTest {
     public static void main(String[] args) {
         Cardapio cardapio = new Cardapio();
 
+        ItemCardapio itemCardapio1 = new ItemCardapio();
+
+        itemCardapio1.descricao = "Fries";
+        itemCardapio1.preco = 35;
+
+        cardapio.adicionarItem(itemCardapio1);
+
+        cardapio.imprimirItensCardapio(35,120);
 
     }
 }

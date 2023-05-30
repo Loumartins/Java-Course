b4cb9b1d39a7e34dd43156463985b36b7d3b9731
Luiz <luizhmartins11@gmail.com>
2023-05-30 15:03:12 -0300

    estudos iniciais sobre varags

--- src/Cap8/TrabalhandoComArrays/aula10/domain/ServicoCobranca.java
+++ src/Cap8/TrabalhandoComArrays/aula10/domain/ServicoCobranca.java
@@ -1,7 +1,7 @@
 package Cap8.TrabalhandoComArrays.aula10.domain;
 
 public class ServicoCobranca {
-    public void pagar(Fatura fatura,  String[] emailsCobranca){
+    public void pagar(Fatura fatura,  String... emailsCobranca){
         System.out.printf("Fatura %d, no valor total de R$%.2f, foi paga!%n",
                 fatura.numero, fatura.valorTotal);
 
---
+++ src/Cap8/TrabalhandoComArrays/aula10/test/Principal.java
@@ -0,0 +1,27 @@
+package Cap8.TrabalhandoComArrays.aula10.test;
+
+import Cap8.TrabalhandoComArrays.aula10.domain.Fatura;
+import Cap8.TrabalhandoComArrays.aula10.domain.ServicoCobranca;
+
+public class Principal {
+    public static void main(String[] args) {
+        Fatura fatura = new Fatura();
+
+        fatura.numero = 123;
+        fatura.valorTotal = 1_293.55;
+
+        ServicoCobranca servicoCobranca = new ServicoCobranca();
+
+//        String[] emailsCobranca = {"joao@algaworks.com", "maria@algaworks.com"};
+//
+//        servicoCobranca.pagar(fatura, emailsCobranca);
+
+        //utlizando varags
+        //servicoCobranca.pagar(fatura, "joao@algaworks.com", "maria@algaworks.com");
+
+        //passando apenas um parametro
+        servicoCobranca.pagar(fatura);
+
+
+    }
+}

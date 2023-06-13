69fd7a7a5261de5c0b5690f0fa7e40eb401c0f06
Luiz <luizhmartins11@gmail.com>
2023-06-13 16:23:40 -0300

    realizando validações dos argumentos dos construtores

--- src/Cap10/construtores/domain/Produto.java
+++ src/Cap10/construtores/domain/Produto.java
@@ -1,5 +1,7 @@
 package Cap10.construtores.domain;
 
+import java.util.Objects;
+
 public class Produto {
    static final int QUANTIDADE_INICIAL_PADRAO = 100;
     public int quantidadeEstoque;
@@ -9,11 +11,17 @@ public class Produto {
     public Produto() {
     }
     public Produto(String nome) {
+        Objects.requireNonNull(nome, "Nome é obrigatório");
         this.nome = nome;
         this.quantidadeEstoque = QUANTIDADE_INICIAL_PADRAO;
     }
     public Produto(String nome, int quantidadeEstoque) {
+        Objects.requireNonNull(nome, "Nome é obrigatório");
         this.nome = nome;
+
+        if (quantidadeEstoque < 0){
+            throw new IllegalArgumentException("Estoque inicial nao pode ser negativo");
+        }
         this.quantidadeEstoque = quantidadeEstoque;
 //        System.out.println("Construindo um produto");
     }

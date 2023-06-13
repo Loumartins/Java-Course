2eda673477b28012135428936c5f573c1f776c39
Luiz <luizhmartins11@gmail.com>
2023-06-13 16:06:50 -0300

    refatorando classe produto e criando uma sobrecarga de construtores.

--- src/Cap10/construtores/domain/Produto.java
+++ src/Cap10/construtores/domain/Produto.java
@@ -1,15 +1,18 @@
 package Cap10.construtores.domain;
 
 public class Produto {
-    static final int QUANTIDADE_INICIAL_PADRAO = 100;
+   // static final int QUANTIDADE_INICIAL_PADRAO = 100;
     public int quantidadeEstoque;
     public String nome;
 
-
+    //sobrecarga de construtor
     public Produto(String nome) {
         this.nome = nome;
-        this.quantidadeEstoque = QUANTIDADE_INICIAL_PADRAO;
-
+    }
+    public Produto(String nome, int quantidadeEstoque) {
+        this.nome = nome;
+        this.quantidadeEstoque = quantidadeEstoque;
 //        System.out.println("Construindo um produto");
     }
+
 }
--- src/Cap10/construtores/test/Principal.java
+++ src/Cap10/construtores/test/Principal.java
@@ -5,7 +5,7 @@ import Cap10.construtores.domain.Produto;
 public class Principal {
     public static void main(String[] args) {
         Produto produto1 = new Produto( "Picanha 1kg (Peça)");
-        Produto produto2 = new Produto( "Maminha 1kg (Peça)");
+        Produto produto2 = new Produto( "Maminha 1kg (Peça)",10);
 
         System.out.println(produto1.quantidadeEstoque);
         System.out.println(produto2.quantidadeEstoque);

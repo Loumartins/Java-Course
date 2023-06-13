d3f2a5be8fe6b6bdc23cf8843e078fe7cc7ae41e
Luiz <luizhmartins11@gmail.com>
2023-06-13 09:49:48 -0300

    estudos sobre gerenciamento e informação da memoria heap

---
+++ src/Cap9/gerenciamentoDeMemoria/main/Teste.java
@@ -0,0 +1,23 @@
+package Cap9.gerenciamentoDeMemoria.main;
+
+public class Teste {
+    public static void main(String[] args) {
+        imprimirUsoMemoria();
+    }
+    public static void imprimirUsoMemoria(){
+        //Imprimindo memoria maxima disponivel
+        System.out.printf("Maxima: %s%n",
+                converterEmMegabytes(Runtime.getRuntime().maxMemory()));
+
+        //imprimindo total de memoria empenhada
+        System.out.printf("Empenhada: %s%n",
+                converterEmMegabytes(Runtime.getRuntime().totalMemory()));
+
+        //imprimindo memoria disponivel
+        System.out.printf("Disponivel: %s%n",
+                converterEmMegabytes(Runtime.getRuntime().freeMemory()));
+    }
+    public static String converterEmMegabytes(long bytes){
+        return String.format("%.2fMB", bytes / 1024d / 1024d);
+    }
+}

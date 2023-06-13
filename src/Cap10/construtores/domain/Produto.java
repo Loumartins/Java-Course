6a7780df026ef5c38ebd47b19debb4560d3314b0
Luiz <luizhmartins11@gmail.com>
2023-06-13 15:42:44 -0300

    refatorando classe principal e introduzindo variavel estatica na classe domain

--- src/Cap10/construtores/domain/Produto.java
+++ src/Cap10/construtores/domain/Produto.java
@@ -1,11 +1,15 @@
 package Cap10.construtores.domain;
 
 public class Produto {
+    static final int QUANTIDADE_INICIAL_PADRAO = 100;
     public int quantidadeEstoque;
     public String nome;
-    public Produto(int quantidadeEstoque, String nome) {
-        this.quantidadeEstoque = quantidadeEstoque;
+
+
+    public Produto(String nome) {
         this.nome = nome;
-        System.out.println("Construindo um produto");
+        this.quantidadeEstoque = QUANTIDADE_INICIAL_PADRAO;
+
+//        System.out.println("Construindo um produto");
     }
 }

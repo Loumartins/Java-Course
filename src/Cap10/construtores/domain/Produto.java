9785ff6161d26c42ae3a3c9a6a6fc59d0a11f61a
Luiz <luizhmartins11@gmail.com>
2023-06-14 10:14:20 -0300

    implementado variavel final codigo

--- src/Cap10/construtores/domain/Produto.java
+++ src/Cap10/construtores/domain/Produto.java
@@ -1,27 +1,33 @@
 package Cap10.construtores.domain;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class Produto {
-   static final int QUANTIDADE_INICIAL_PADRAO = 100;
+
+    static final int QUANTIDADE_INICIAL_PADRAO = 100;
+    public final String codigo;
+
     public int quantidadeEstoque;
     public String nome;
 
-    //sobrecarga de construtor
     public Produto() {
+        this("Sem nome");
     }
+
     public Produto(String nome) {
         this(nome, QUANTIDADE_INICIAL_PADRAO);
     }
+
     public Produto(String nome, int quantidadeEstoque) {
         Objects.requireNonNull(nome, "Nome é obrigatório");
-        this.nome = nome;
 
         if (quantidadeEstoque < 0){
             throw new IllegalArgumentException("Estoque inicial nao pode ser negativo");
         }
+        this.nome = nome;
         this.quantidadeEstoque = quantidadeEstoque;
-//        System.out.println("Construindo um produto");
+        this.codigo = UUID.randomUUID().toString();
     }
 
 
--- src/Cap10/construtores/test/Principal.java
+++ src/Cap10/construtores/test/Principal.java
@@ -6,8 +6,14 @@ public class Principal {
     public static void main(String[] args) {
         Produto produto1 = new Produto( "Picanha 1kg (Peça)");
         Produto produto2 = new Produto( "Maminha 1kg (Peça)",10);
+        Produto produto3 = new Produto();
 
         System.out.println(produto1.quantidadeEstoque);
         System.out.println(produto2.quantidadeEstoque);
+
+        System.out.println(produto1.codigo);
+        System.out.println(produto2.codigo);
+        System.out.println(produto3.codigo);
+
     }
 }

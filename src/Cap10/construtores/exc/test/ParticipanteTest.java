c3af339d96735e8541afc3ecb03e8db76598eabc
Luiz <luizhmartins11@gmail.com>
2023-06-14 09:37:25 -0300

    implementando codigo para teste

--- src/Cap10/construtores/exc/test/ParticipanteTest.java
+++ src/Cap10/construtores/exc/test/ParticipanteTest.java
@@ -5,7 +5,12 @@ import Cap10.construtores.exc.domain.Participante;
 public class ParticipanteTest {
     public static void main(String[] args) {
 
-        Participante participante1 = new Participante(null);
-        System.out.println(participante1.nome);
+
+        Participante participante1 = new Participante("Fernando");
+        participante1.imprimir();
+
+        Participante participante2 = new Participante("Jose", 35);
+        participante2.imprimir();
+
     }
 }

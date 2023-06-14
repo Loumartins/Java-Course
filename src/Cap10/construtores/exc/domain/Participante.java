8568ceb51e1b1b9ab5d790514c6faf61d5bc4898
Luiz <luizhmartins11@gmail.com>
2023-06-14 09:32:36 -0300

    gerando as validações de argumento

--- src/Cap10/construtores/exc/domain/Participante.java
+++ src/Cap10/construtores/exc/domain/Participante.java
@@ -1,15 +1,23 @@
 package Cap10.construtores.exc.domain;
 
+import java.util.Objects;
+
 public class Participante {
     public String nome;
     public int saldoDePontos;
 
+
     public Participante(String nome) {
-        this.nome = nome;
+        this(nome,0);
+
     }
 
     public Participante(String nome, int saldoDePontosInicial) {
+        Objects.requireNonNull(nome, "Nome nao pode ser nulo");
         this.nome = nome;
+        if (saldoDePontosInicial < 0 ){
+            throw new IllegalArgumentException("Saldo inicial de ponto nao pode ser negativo");
+        }
         this.saldoDePontos = saldoDePontosInicial;
     }
 }

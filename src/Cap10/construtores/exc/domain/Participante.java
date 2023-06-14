12945c5d6612f5db10a036d27244e666bb4d543b
Luiz <luizhmartins11@gmail.com>
2023-06-14 09:24:22 -0300

    criação dos construtores da classe Participante

--- src/Cap10/construtores/exc/domain/Participante.java
+++ src/Cap10/construtores/exc/domain/Participante.java
@@ -1,4 +1,16 @@
 package Cap10.construtores.exc.domain;
 
 public class Participante {
+    public String nome;
+    public int saldoDePontos;
+
+    public Participante(String nome) {
+        this.nome = nome;
+    }
+
+    public Participante(String nome, int saldoDePontosInicial) {
+        this.nome = nome;
+        this.saldoDePontos = saldoDePontosInicial;
+    }
 }
+

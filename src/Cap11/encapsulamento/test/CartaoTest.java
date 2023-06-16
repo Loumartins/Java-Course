96b4bca9263c3f33188d77780f6b459fa26a1c99
Luiz <luizhmartins11@gmail.com>
2023-06-16 10:44:25 -0300

    implementando classe test

---
+++ src/Cap11/encapsulamento/test/CartaoTest.java
@@ -0,0 +1,25 @@
+package Cap11.encapsulamento.test;
+
+import Cap11.encapsulamento.domain.*;
+
+public class CartaoTest {
+    public static void main(String[] args) {
+        var supermercado = new Estabelecimento("Supermercado do Zé");
+        var cartao = new Cartao("Joao Souza Silva");
+
+        var servicoDeDeposito = new ServicoDeDepositoPix();
+
+        Recibo reciboDeposito = servicoDeDeposito.efeturarDeposito(cartao, 500);
+
+        var servicoDePagamento = new ServicoDePagamentoOnline();
+        Recibo reciboPagamento = servicoDePagamento.efetuarPagamento(supermercado, cartao, 200);
+
+        reciboDeposito.imprimir();
+
+        reciboPagamento.imprimir();
+
+
+        System.out.println(cartao.saldo);
+    }
+}
+

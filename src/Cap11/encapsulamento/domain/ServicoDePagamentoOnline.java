12cdc6fa122e65d7e541dc9d65dcab4c4f043641
Luiz <luizhmartins11@gmail.com>
2023-06-16 10:38:26 -0300

    implementando metodo para efetuar pagamento

--- src/Cap11/encapsulamento/domain/ServicoDePagamentoOnline.java
+++ src/Cap11/encapsulamento/domain/ServicoDePagamentoOnline.java
@@ -1,4 +1,16 @@
 package Cap11.encapsulamento.domain;
 
 public class ServicoDePagamentoOnline {
+
+    public Recibo efetuarPagamento(Estabelecimento estabelecimento, Cartao cartao, double valor){
+
+        if (cartao.saldo < valor){
+            throw new RuntimeException("Saldo insuficiente para pagamento");
+        }
+        cartao.saldo -= valor;
+
+        // TODO realiza outras logicas para efetuar o pagamento ao estabelecimento
+
+        return new Recibo(cartao.titular, "Pagamento", valor);
+    }
 }

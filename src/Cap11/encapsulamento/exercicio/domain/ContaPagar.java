35297581c9deb160302c3ee07343faee4f43310e
Luiz <luizhmartins11@gmail.com>
2023-06-19 11:28:11 -0300

    implementando modificados de acesso

--- src/Cap11/encapsulamento/exercicio/domain/ContaPagar.java
+++ src/Cap11/encapsulamento/exercicio/domain/ContaPagar.java
@@ -7,4 +7,48 @@ public class ContaPagar {
     private String dataVencimento;
     private boolean pago;
 
+    private Fornecedor fornecedor;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public boolean isPago() {
+        return pago;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public void pagar(){
+        if (isPago() == true){
+            throw new RuntimeException("Nao é possivel pagar uma conta que ja está paga");
+        }
+
+    }
 }

27f64f9d1a2f79e1ce0eeeed56152b8c595e306d
Luiz <luizhmartins11@gmail.com>
2023-06-19 10:37:34 -0300

    implementando classes e realizando refatorações

--- src/Cap11/encapsulamento/domain/Cartao.java
+++ src/Cap11/encapsulamento/domain/Cartao.java
@@ -18,7 +18,14 @@ public class Cartao {
         return saldo;
     }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
+    public void debitar (double valorDebito){
+        saldo-=valorDebito;
+    }
+    public void depositar (double valorDeposito){
+        if (valorDeposito < VALOR_MINIMO_DEPOSITO){
+            throw new IllegalArgumentException(String.format
+                    ("Valor de deposito nao pode ser menor que %.2f", Cartao.VALOR_MINIMO_DEPOSITO));
+        }
+        saldo+=valorDeposito - TARIFA_DEPOSITO;
     }
 }
--- src/Cap11/encapsulamento/domain/ServicoDeDepositoPix.java
+++ src/Cap11/encapsulamento/domain/ServicoDeDepositoPix.java
@@ -5,11 +5,8 @@ public class ServicoDeDepositoPix {
     public Recibo efeturarDeposito(Cartao cartao, double valorDeposito){
         // TODO faz cobrança do valor no pix
 
-        if (valorDeposito < Cartao.VALOR_MINIMO_DEPOSITO){
-            throw new IllegalArgumentException(String.format
-                    ("Valor de deposito nao pode ser menor que %.2f", Cartao.VALOR_MINIMO_DEPOSITO));
-        }
-        cartao.setSaldo(cartao.getSaldo() + valorDeposito - Cartao.TARIFA_DEPOSITO );
+        cartao.depositar(valorDeposito);
+
         return new Recibo(cartao.getTitular(), "Deposito", valorDeposito);
     }
 }

c71244ada6cdf0cdbfe590e9a6cb4ede77aa3c20
Luiz <luizhmartins11@gmail.com>
2023-06-19 16:19:29 -0300

    refatorando metodo

--- src/Cap11/encapsulamento/domain/ServicoLocacao.java
+++ src/Cap11/encapsulamento/domain/ServicoLocacao.java
@@ -2,8 +2,13 @@ package Cap11.encapsulamento.domain;
 
 public class ServicoLocacao {
     public void confirmarLocacao(Locacao locacao){
-        double totalDiarias = locacao.getVeiculo().getGrupo().getValorDiaria() *
-                locacao.getQuantidadeDiarias();
+
+//        double totalDiarias = locacao.getVeiculo().getGrupo().getValorDiaria() *
+//                locacao.getQuantidadeDiarias();
+
+        //refatorando metodo acima com law-of-demeter
+
+        double totalDiarias = locacao.getValorDiaria();
 
         locacao.getVeiculo().setDisponivel(false);
     }

cdf6de7d8d8f27fa945037b48f1296767d9f871a
Luiz <luizhmartins11@gmail.com>
2023-06-20 10:44:27 -0300

    implementando modificadores de acesso e métodos

--- src/Cap11/encapsulamento/domain/agenda/domain/Agendamento.java
+++ src/Cap11/encapsulamento/domain/agenda/domain/Agendamento.java
@@ -1,4 +1,30 @@
 package Cap11.encapsulamento.domain.agenda.domain;
 
 public class Agendamento {
+    private Horario horario;
+    private String descricao;
+
+    public Agendamento() {
+    }
+
+    public Agendamento(Horario horario, String descricao) {
+        this.horario = horario;
+        this.descricao = descricao;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }

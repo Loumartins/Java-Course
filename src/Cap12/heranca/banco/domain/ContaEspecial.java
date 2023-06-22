595bcc864a1ce1421331ec14b6ad3c0bec2adbc7
Luiz <luizhmartins11@gmail.com>
2023-06-22 14:26:51 -0300

    implementando metodos

--- src/Cap12/heranca/banco/domain/ContaEspecial.java
+++ src/Cap12/heranca/banco/domain/ContaEspecial.java
@@ -5,7 +5,24 @@ public class ContaEspecial extends ContaInvestimento {
     private double tarifaMensal;
     private double limiteChequeEspecial;
 
+    public double getTarifaMensal() {
+        return tarifaMensal;
+    }
+
+    public void setTarifaMensal(double tarifaMensal) {
+        this.tarifaMensal = tarifaMensal;
+    }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
     public void debitarTarifaMensal(){
+
 
     }
 }
--- src/Cap12/heranca/banco/domain/ContaInvestimento.java
+++ src/Cap12/heranca/banco/domain/ContaInvestimento.java
@@ -3,7 +3,13 @@ package Cap12.heranca.banco.domain;
 public class ContaInvestimento extends Conta{
     private double valorTotalRendimentos;
 
-    public void creditarRendimentos(double percentualJuros){
+    public double getValorTotalRendimentos() {
+        return valorTotalRendimentos;
+    }
 
+    public void creditarRendimentos(double percentualJuros){
+        double valorRendimentos = getSaldo() * percentualJuros / 100;
+        this.valorTotalRendimentos+= valorRendimentos;
+        depositar(valorRendimentos);
     }
 }

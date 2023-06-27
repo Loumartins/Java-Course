9e8eab9e3a243b38bc5817f913bc15c4b542edfb
Luiz <luizhmartins11@gmail.com>
2023-06-27 14:54:56 -0300

    implementando metodos na classe Programador

--- src/Cap12/heranca/funcionario/domain/Programador.java
+++ src/Cap12/heranca/funcionario/domain/Programador.java
@@ -1,4 +1,34 @@
 package Cap12.heranca.funcionario.domain;
 
-public class Programador {
+public class Programador extends Funcionario {
+    private double valorBonus;
+    private boolean temBonus;
+
+    public Programador(String nome, double valorHora) {
+        super(nome, valorHora);
+    }
+
+    public double getValorBonus() {
+        return valorBonus;
+    }
+
+    public void setValorBonus(double valorBonus) {
+        this.valorBonus = valorBonus;
+    }
+
+    public boolean isTemBonus() {
+        return temBonus;
+    }
+
+    public void setTemBonus(boolean temBonus) {
+        this.temBonus = temBonus;
+    }
+
+    @Override
+    protected double calcularSalario(int horasTrabalhadas) {
+        if (isTemBonus() == false){
+            return super.calcularSalario(horasTrabalhadas);
+        }
+        return super.calcularSalario(horasTrabalhadas) + getValorBonus();
+    }
 }

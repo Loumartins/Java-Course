89f792ac35d3404a31d921c9e3fd3da50f87b1a8
Luiz <luizhmartins11@gmail.com>
2023-06-27 14:28:17 -0300

    implementando construtor e metodo na classe record

--- src/Cap12/heranca/funcionario/domain/Holerite.java
+++ src/Cap12/heranca/funcionario/domain/Holerite.java
@@ -1,4 +1,18 @@
 package Cap12.heranca.funcionario.domain;
 
-public record Holerite() {
+import java.util.Objects;
+
+public record Holerite(String nome, String mesAno, double valorSalario) {
+
+    public Holerite{
+        Objects.requireNonNull(nome, "O nome nao pode ser nulo");
+        Objects.requireNonNull(mesAno, "O nome nao pode ser nulo");
+        Objects.requireNonNull(valorSalario, "O nome nao pode ser nulo");
+    }
+
+    public void imprimir(){
+        System.out.printf("O nome do funcionário é: %s, o mes é: $s e o valor de salário é de" +
+                "%.2f%n", nome, mesAno, valorSalario);
+    }
+
 }

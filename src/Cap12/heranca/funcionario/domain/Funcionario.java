581ea7afa72723296d2a33a7661ff69ceb1d9f67
Luiz <luizhmartins11@gmail.com>
2023-06-27 14:48:16 -0300

    implementando metodos a classe Funcionario

--- src/Cap12/heranca/funcionario/domain/Funcionario.java
+++ src/Cap12/heranca/funcionario/domain/Funcionario.java
@@ -1,5 +1,6 @@
 package Cap12.heranca.funcionario.domain;
 
+
 public class Funcionario {
     private String nome;
     private double valorHora;
@@ -7,5 +8,38 @@ public class Funcionario {
     public Funcionario(String nome, double valorHora) {
         this.nome = nome;
         this.valorHora = valorHora;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+    protected double calcularSalario(int horasTrabalhadas){
+        double salarioCalculado = horasTrabalhadas * valorHora;
+        return salarioCalculado;
+    }
+
+    public Holerite gerarHolerite(int horasTrabalhadas, String mesAno){
+        double valorSalario = calcularSalario(horasTrabalhadas);
+        return new Holerite(getNome(), mesAno, valorSalario);
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", valorHora=" + valorHora +
+                '}';
     }
 }

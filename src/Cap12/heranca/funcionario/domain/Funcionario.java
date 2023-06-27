026de3b64bb7948f45ecc430817ff8d0b221e598
Luiz <luizhmartins11@gmail.com>
2023-06-27 14:29:14 -0300

    implementando classe Funcionario

---
+++ src/Cap12/heranca/funcionario/domain/Funcionario.java
@@ -0,0 +1,11 @@
+package Cap12.heranca.funcionario.domain;
+
+public class Funcionario {
+    private String nome;
+    private double valorHora;
+
+    public Funcionario(String nome, double valorHora) {
+        this.nome = nome;
+        this.valorHora = valorHora;
+    }
+}

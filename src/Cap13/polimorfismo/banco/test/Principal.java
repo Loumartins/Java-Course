f6cd671c062056d904be6479d03d4bf090a2f7e5
Luiz <luizhmartins11@gmail.com>
2023-06-28 10:47:01 -0300

    criando classe Principal

---
+++ src/Cap13/polimorfismo/banco/test/Principal.java
@@ -0,0 +1,13 @@
+package Cap13.polimorfismo.banco.test;
+
+import Cap13.polimorfismo.banco.domain.ContaEspecial;
+import Cap13.polimorfismo.banco.domain.Titular;
+
+public class Principal {
+    public static void main(String[] args) {
+        Titular titular = new Titular("Joao da Silva", "123.432.556-43");
+        ContaEspecial contaEspecial = new ContaEspecial(1234,9999, titular);
+        contaEspecial.setLimiteChequeEspecial(1000);
+        contaEspecial.depositar(100);
+    }
+}

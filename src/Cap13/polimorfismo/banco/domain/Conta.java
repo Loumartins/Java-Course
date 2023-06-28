d987e279ec7d9f499313cb7f64ba416afd1c40ca
Luiz <luizhmartins11@gmail.com>
2023-06-28 10:23:50 -0300

    implementando metodos na classe Conta

--- src/Cap13/polimorfismo/banco/domain/Conta.java
+++ src/Cap13/polimorfismo/banco/domain/Conta.java
@@ -6,5 +6,44 @@ public class Conta {
     private double saldo;
     private Titular titular;
 
+    public Conta(int agencia, int numero, Titular titular) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.titular = titular;
+    }
 
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public void sacar(double valorSaque){
+        if (valorSaque > getSaldo()){
+            throw new IllegalArgumentException("Valor do saque é maior que o valor de saldo");
+        }
+        saldo-=valorSaque;
+    }
+    public void depositar(double valorDeposito){
+        if (valorDeposito <= 0){
+            throw new IllegalArgumentException("Valor de deposito nao pode ser menor ou igual a 0");
+        }
+        saldo+=valorDeposito;
+    }
+    public void imprimirDemonstrativo(){
+        System.out.println("Nome: " + titular.getNome());
+        System.out.println("Agencia: " + agencia);
+        System.out.println("Numero: " + numero);
+        System.out.println("Saldo: " + saldo);
+    }
 }

a085f56b529ffb15ce68389251a282094c749faf
Luiz <luizhmartins11@gmail.com>
2023-06-29 19:20:39 -0300

    criando classe NotaFiscalProduto

---
+++ src/Cap13/polimorfismo/notaFiscal/domain/NotaFiscalProduto.java
@@ -0,0 +1,14 @@
+package Cap13.polimorfismo.notaFiscal.domain;
+
+public class NotaFiscalProduto extends NotaFiscal {
+    private double valorFrete;
+
+    public NotaFiscalProduto(String descricao, double valorTotal, double valorFrete) {
+        super(descricao, valorTotal);
+        this.valorFrete = valorFrete;
+    }
+
+    public double getValorFrete() {
+        return valorFrete;
+    }
+}

d11e9e5eab887dda123b7625e0a8fcc173d45d70
Luiz <luizhmartins11@gmail.com>
2023-06-29 10:19:31 -0300

    implementando metodo na classe CaixaEletronico

--- src/Cap13/polimorfismo/banco/domain/CaixaEletronico.java
+++ src/Cap13/polimorfismo/banco/domain/CaixaEletronico.java
@@ -5,6 +5,7 @@ import Cap13.polimorfismo.banco.domain.ContaEspecial;
 public class CaixaEletronico {
 
     public static final double TARIFA_TRANSFERENCIA = 10;
+    public static final double TARIFA_IMPRESSAO_DEMONSTRATIVO = 5;
     public void transferir(Conta contaOrigem, Conta contaDestino,
                            double valorTransferencia){
         System.out.printf("Transferindo R$%.2f da conta %d/%d para %d/%d%n"
@@ -13,5 +14,10 @@ public class CaixaEletronico {
 
         contaOrigem.sacar(valorTransferencia + TARIFA_TRANSFERENCIA);
         contaDestino.depositar(valorTransferencia);
+    }
+
+    public void imprimirDemonstrativo(Conta conta){
+        System.out.printf("Custo da impressão: R$:%.2f%n", TARIFA_IMPRESSAO_DEMONSTRATIVO);
+        conta.sacar(TARIFA_IMPRESSAO_DEMONSTRATIVO);
     }
 }

026c8880104cdff34db51a21967e0ded1ed4f0b1
Luiz <luizhmartins11@gmail.com>
2023-06-29 19:18:17 -0300

    criando classe NotaFiscal

---
+++ src/Cap13/polimorfismo/notaFiscal/domain/NotaFiscal.java
@@ -0,0 +1,19 @@
+package Cap13.polimorfismo.notaFiscal.domain;
+
+public class NotaFiscal {
+    private String descricao;
+    private double valorTotal;
+
+    public NotaFiscal(String descricao, double valorTotal) {
+        this.descricao = descricao;
+        this.valorTotal = valorTotal;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+}

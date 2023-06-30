c0a8659ed9f6d7859cbc957343a2c0340cff5321
Luiz <luizhmartins11@gmail.com>
2023-06-30 11:03:07 -0300

    implementando metodo na classe GestorImposto

--- src/Cap13/polimorfismo/exercicios/impostos/domain/GestorImposto.java
+++ src/Cap13/polimorfismo/exercicios/impostos/domain/GestorImposto.java
@@ -7,9 +7,10 @@ public class GestorImposto {
         return valorTotalImpostos;
     }
 
-    public void adicionarPessoas(Pessoa... pessoas){
+    public void adicionar(Pessoa... pessoas){
         for (Pessoa pessoa : pessoas){
-            pessoa.calcularImposto();
+            System.out.printf("Impostos devidos de %s: %.2f%n", pessoa.getNome(), pessoa.calcularImposto());
+            valorTotalImpostos+= pessoa.calcularImposto();
         }
     }
 }

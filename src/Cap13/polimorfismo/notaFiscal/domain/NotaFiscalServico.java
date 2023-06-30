a94c76d1b88ab86e763ea6fef85ca15444386ccb
Luiz <luizhmartins11@gmail.com>
2023-06-30 09:24:10 -0300

    realizando override do metodo calcularImpostos

--- src/Cap13/polimorfismo/notaFiscal/domain/NotaFiscalServico.java
+++ src/Cap13/polimorfismo/notaFiscal/domain/NotaFiscalServico.java
@@ -1,6 +1,9 @@
 package Cap13.polimorfismo.notaFiscal.domain;
 
 public class NotaFiscalServico extends NotaFiscal    {
+
+    public static final int VALOR_MAXIMO_ISENCAO_FISCAL = 1000;
+    public static final double ALIQUOTA_IMPOSTOS = 0.18;
     private boolean intermunicipal;
 
     public NotaFiscalServico(String descricao, double valorTotal, boolean intermunicipal) {
@@ -12,4 +15,18 @@ public class NotaFiscalServico extends NotaFiscal    {
         return intermunicipal;
     }
 
+
+    @Override
+    public double calcularImpostos() {
+        double valorImpostos = getValorTotal() * ALIQUOTA_IMPOSTOS;
+
+        if (isIsentoImpostos()){
+            valorImpostos = 0;
+        }
+        return valorImpostos;
+    }
+
+    private boolean isIsentoImpostos() {
+        return isIntermunicipal() && getValorTotal() <= VALOR_MAXIMO_ISENCAO_FISCAL;
+    }
 }

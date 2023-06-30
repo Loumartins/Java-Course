589449634a48d20ec8551461716d8d9a8c576443
Luiz <luizhmartins11@gmail.com>
2023-06-30 11:03:31 -0300

    implementando classe ImpostoTest

---
+++ src/Cap13/polimorfismo/exercicios/impostos/test/ImpostoTest.java
@@ -0,0 +1,18 @@
+package Cap13.polimorfismo.exercicios.impostos.test;
+
+import Cap13.polimorfismo.exercicios.impostos.domain.GestorImposto;
+import Cap13.polimorfismo.exercicios.impostos.domain.PessoaFisica;
+
+public class ImpostoTest {
+    public static void main(String[] args) {
+        var gestorImpostos = new GestorImposto();
+
+        var joao = new PessoaFisica("Joao da Silva", 45_000);
+        var maria = new PessoaFisica("Maria de Souza", 180_000);
+
+        gestorImpostos.adicionar(joao);
+        gestorImpostos.adicionar(maria);
+
+        System.out.printf("Total de impostos: %.2f%n", gestorImpostos.getValorTotalImpostos());
+    }
+}

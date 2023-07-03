3ad73c2f9b1b11923efc1e295fc8586781884c70
Luiz <luizhmartins11@gmail.com>
2023-07-03 11:37:24 -0300

    implementando classe Pix

---
+++ src/Cap14/interfaces/contasPagar/domain/Pix.java
@@ -0,0 +1,20 @@
+package Cap14.interfaces.contasPagar.domain;
+
+import Cap14.interfaces.contasPagar.domain.interfaces.DocumentoPagavel;
+import Cap14.interfaces.contasPagar.domain.interfaces.MetodoPagamento;
+
+public class Pix implements MetodoPagamento {
+
+
+    @Override
+    public void pagar(DocumentoPagavel documento) {
+        Beneficiario beneficiario =  documento.getBeneficiario();
+
+        if (beneficiario.naoPossuiChavePix()){
+            throw new RuntimeException("Beneficiario nao possui chave Pix");
+        }
+
+
+        System.out.printf("DEBUG: Efetuando PIX para %s no valor de %.2f com a chave %s%n", beneficiario.getNome(),documento.getValorTotal(), beneficiario.getChavePix());
+    }
+}
--- src/Cap14/interfaces/contasPagar/domain/ServicoContaPagar.java
+++ src/Cap14/interfaces/contasPagar/domain/ServicoContaPagar.java
@@ -5,11 +5,6 @@ import Cap14.interfaces.contasPagar.domain.interfaces.DocumentoPagavel;
 public class ServicoContaPagar {
 
     public void pagar(DocumentoPagavel documento){
-        Beneficiario beneficiario =  documento.getBeneficiario();
 
-        if (beneficiario.naoPossuiChavePix()){
-            throw new RuntimeException("Beneficiario nao possui chave Pix");
-        }
-        System.out.printf("DEBUG: Efetuando PIX para %s no valor de %.2f com a chave %s%n", beneficiario.getNome(),documento.getValorTotal(), beneficiario.getChavePix());
     }
 }

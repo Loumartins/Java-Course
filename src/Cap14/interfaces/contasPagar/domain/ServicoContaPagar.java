cf8c2f44ebffd30093a313548e4520d3e7df270e
Luiz <luizhmartins11@gmail.com>
2023-07-03 11:39:06 -0300

    refatorando metodo pagar() da classe ServicoContaPagar

--- src/Cap14/interfaces/contasPagar/domain/ServicoContaPagar.java
+++ src/Cap14/interfaces/contasPagar/domain/ServicoContaPagar.java
@@ -1,10 +1,15 @@
 package Cap14.interfaces.contasPagar.domain;
 
 import Cap14.interfaces.contasPagar.domain.interfaces.DocumentoPagavel;
+import Cap14.interfaces.contasPagar.domain.interfaces.MetodoPagamento;
 
 public class ServicoContaPagar {
 
     public void pagar(DocumentoPagavel documento){
+        MetodoPagamento metodoPagamento = new Pix();
 
+
+
+        metodoPagamento.pagar(documento);
     }
 }

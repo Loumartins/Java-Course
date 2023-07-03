1e24bbfb9d1f311afdcad2a3c6e317be66db685d
Luiz <luizhmartins11@gmail.com>
2023-07-03 11:19:29 -0300

    implementando classe Principal

--- src/Cap14/interfaces/contasPagar/test/Principal.java
+++ src/Cap14/interfaces/contasPagar/test/Principal.java
@@ -1,11 +1,17 @@
 package Cap14.interfaces.contasPagar.test;
 
+import Cap14.interfaces.contasPagar.domain.Beneficiario;
+import Cap14.interfaces.contasPagar.domain.Holerite;
 import Cap14.interfaces.contasPagar.domain.ServicoContaPagar;
 
 public class Principal {
     public static void main(String[] args) {
         ServicoContaPagar servicoContaPagar = new ServicoContaPagar();
 
-        servicoContaPagar.pagar();
+        Beneficiario funcionario = new Beneficiario("Joao da Silva", "232.323.232-34", "2323-4");
+
+        Holerite documento = new Holerite(funcionario, 100, 168);
+
+        servicoContaPagar.pagar(documento);
     }
 }

6a006f7839f236cc9ee3382f88b668a0ed685d3c
Luiz <luizhmartins11@gmail.com>
2023-07-12 15:02:22 -0300

    refatorando metodos na classe ServicoFinanciamento

--- src/Cap14/interfaces/empresa/servico/ServicoFinanciamento.java
+++ src/Cap14/interfaces/empresa/servico/ServicoFinanciamento.java
@@ -4,7 +4,14 @@ import Cap14.interfaces.empresa.domain.Empresa;
 
 public class ServicoFinanciamento {
     public void solicitarFinanciamento(Empresa empresa, double valorSolicitado){
+        double limiteAprovado = empresa.calcularLimiteAprovado();
 
+        if (limiteAprovado < valorSolicitado){
+            throw new RuntimeException(String.format("Financiamento nao aprovado. Limite máximo de" +
+                    "%.2f ", limiteAprovado));
+        }
+
+        System.out.printf("DEBUG: Financiamento aprovado.Limite máximo de %.2f%n", limiteAprovado);
     }
     public double consultarLimiteAprovado(Empresa empresa){
         return 0;

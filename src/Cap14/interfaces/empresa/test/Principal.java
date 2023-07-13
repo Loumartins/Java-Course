46c9bfe3130258afe2f1dc63ae56ba90f133faa2
Luiz <luizhmartins11@gmail.com>
2023-07-13 13:47:00 -0300

    criando classe Principal

---
+++ src/Cap14/interfaces/empresa/test/Principal.java
@@ -0,0 +1,15 @@
+package Cap14.interfaces.empresa.test;
+
+import Cap14.interfaces.empresa.domain.Fazenda;
+import Cap14.interfaces.empresa.domain.Industria;
+import Cap14.interfaces.empresa.servico.ServicoFinanciamento;
+
+public class Principal {
+    public static void main(String[] args) {
+        var servicoFinanciamento = new ServicoFinanciamento();
+
+        var fazenda = new Fazenda("Dona Benedita", 5_000_000, 5);
+        var industria = new Industria("Alimentos da Vovó", 900_000, true);
+        servicoFinanciamento.solicitarFinanciamento(fazenda, 600_000);
+    }
+}

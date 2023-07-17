c886e317f43874f23041ab5e887163206704b8ea
Luiz <luizhmartins11@gmail.com>
2023-07-17 15:52:16 -0300

    adicionando classe de ServicoPagamentoBonus

---
+++ src/Cap14/interfaces/empresa/servico/ServicoPagamentoBonus.java
@@ -0,0 +1,11 @@
+package Cap14.interfaces.empresa.servico;
+
+import Cap14.interfaces.empresa.domain.PessoaBonificavel;
+
+public class ServicoPagamentoBonus {
+    public void pagar(PessoaBonificavel pessoaBonificavel, double percentualMetaAlcancada){
+        double valor = pessoaBonificavel.calcularBonus(percentualMetaAlcancada);
+
+        System.out.printf("DEBUG: Bônus pago no valor de R$%.2f", valor);
+    }
+}

b2d64461fe66aeae5dca95f0c5905198db1c5e1b
Luiz <luizhmartins11@gmail.com>
2023-07-17 12:33:29 -0300

    refatorando metodo

--- src/Cap14/interfaces/empresa/servico/ServicoFinanciamento.java
+++ src/Cap14/interfaces/empresa/servico/ServicoFinanciamento.java
@@ -5,13 +5,14 @@ import Cap14.interfaces.empresa.domain.ClienteFinanciavel;
 public class ServicoFinanciamento {
     public void solicitarFinanciamento(ClienteFinanciavel clienteFinanciavel, double valorSolicitado){
         double limiteAprovado = clienteFinanciavel.calcularLimiteAprovado();
+        double jurosCalculado = clienteFinanciavel.calcularJuros(valorSolicitado);
 
         if (limiteAprovado < valorSolicitado){
             throw new RuntimeException(String.format("Financiamento nao aprovado. Limite máximo de" +
                     "%.2f ", limiteAprovado));
         }
 
-        System.out.printf("DEBUG: Financiamento aprovado.Limite máximo de %.2f%n", limiteAprovado);
+        System.out.printf("DEBUG: Financiamento aprovado no valor de R$%.2f com juros de %.2f  .Limite máximo de %.2f%n", valorSolicitado, jurosCalculado,  limiteAprovado);
     }
     public double consultarLimiteAprovado(ClienteFinanciavel clienteFinanciavel){
         return clienteFinanciavel.calcularLimiteAprovado();

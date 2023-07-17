1b57c9aa987c5b14f52d25b5040ea26135b0b730
Luiz <luizhmartins11@gmail.com>
2023-07-17 12:33:03 -0300

    implementando um metodo default na interface ClienteFinanciavel

--- src/Cap14/interfaces/empresa/domain/ClienteFinanciavel.java
+++ src/Cap14/interfaces/empresa/domain/ClienteFinanciavel.java
@@ -3,4 +3,13 @@ package Cap14.interfaces.empresa.domain;
 public interface ClienteFinanciavel {
 
     double calcularLimiteAprovado();
+
+    default double calcularJuros(double valorSolicitado) {
+        if (valorSolicitado <= 100_000){
+            return 1.0;
+        } else if (valorSolicitado <= 1_000_000){
+            return 1.5;
+        }
+        return 2.0;
+    }
 }

a04db8cbb19270e636d57d21309dfec5f02e9204
Luiz <luizhmartins11@gmail.com>
2023-07-18 13:29:22 -0300

    implementando classe ServicoPropostaSeguro

---
+++ src/Cap14/interfaces/seguro/servicos/ServicoPropostaSeguro.java
@@ -0,0 +1,13 @@
+package Cap14.interfaces.seguro.servicos;
+
+import Cap14.interfaces.seguro.interfaces.BemSeguravel;
+
+public class ServicoPropostaSeguro {
+    public void emitir(BemSeguravel bemSeguravel){
+        System.out.println("--------------");
+        System.out.println("Proposta de seguro");
+        System.out.println("--------------");
+        bemSeguravel.descrever();
+        System.out.printf("Prêmio: %.2f%n", bemSeguravel.calcularValorPremio());
+    }
+}

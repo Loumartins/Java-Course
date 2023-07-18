07950391837e7a9f7f8bb791a90d83a9f29d1355
Luiz <luizhmartins11@gmail.com>
2023-07-18 12:42:59 -0300

    adicionando construtores, variaveis e modificadores de acesso na classe VeiculoAutomotor.

--- src/Cap14/interfaces/seguro/dominio/VeiculoAutomotor.java
+++ src/Cap14/interfaces/seguro/dominio/VeiculoAutomotor.java
@@ -1,4 +1,39 @@
 package Cap14.interfaces.seguro.dominio;
 
-public class VeiculoAutomotor {
+import Cap14.interfaces.seguro.interfaces.BemSeguravel;
+
+public abstract class VeiculoAutomotor implements BemSeguravel {
+    private String modelo;
+    private double valorMercado;
+    private int anoFabricacao;
+
+    public VeiculoAutomotor(String modelo, double valorMercado, int anoFabricacao) {
+        this.modelo = modelo;
+        this.valorMercado = valorMercado;
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getValorMercado() {
+        return valorMercado;
+    }
+
+    public void setValorMercado(double valorMercado) {
+        this.valorMercado = valorMercado;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
 }

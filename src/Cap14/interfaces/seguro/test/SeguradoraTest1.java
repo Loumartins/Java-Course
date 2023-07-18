15f87f70b989586474798fe3c3b849ddd8f40e8c
Luiz <luizhmartins11@gmail.com>
2023-07-18 13:55:30 -0300

    implementando código na classe SeguradoraTest1

--- src/Cap14/interfaces/seguro/test/SeguradoraTest1.java
+++ src/Cap14/interfaces/seguro/test/SeguradoraTest1.java
@@ -1,5 +1,7 @@
 package Cap14.interfaces.seguro.test;
 
+import Cap14.interfaces.seguro.dominio.Caminhao;
+import Cap14.interfaces.seguro.dominio.CarroParticular;
 import Cap14.interfaces.seguro.dominio.ImovelResidencial;
 import Cap14.interfaces.seguro.servicos.ServicoPropostaSeguro;
 
@@ -7,8 +9,14 @@ public class SeguradoraTest1 {
     public static void main(String[] args) {
         ServicoPropostaSeguro servicoPropostaSeguro = new ServicoPropostaSeguro();
 
+        var carroParticular = new CarroParticular("HR-V", 150_000, 2022);
+        var caminhao = new Caminhao("Actros", 780_000, 2021, 4);
+
+
         ImovelResidencial imovelResidencial = new ImovelResidencial(2_300_000, 320);
 
         servicoPropostaSeguro.emitir(imovelResidencial);
+        servicoPropostaSeguro.emitir(carroParticular);
+        servicoPropostaSeguro.emitir(caminhao);
     }
 }

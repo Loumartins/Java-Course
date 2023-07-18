58f83da5625fea3d0f287f434832bb37e5fe5a18
Luiz <luizhmartins11@gmail.com>
2023-07-18 13:36:04 -0300

    implementando classe CarroParticular

---
+++ src/Cap14/interfaces/seguro/dominio/CarroParticular.java
@@ -0,0 +1,19 @@
+package Cap14.interfaces.seguro.dominio;
+
+public class CarroParticular extends VeiculoAutomotor {
+
+
+    public CarroParticular(String modelo, double valorMercado, int anoFabricacao) {
+        super(modelo, valorMercado, anoFabricacao);
+    }
+
+    @Override
+    public double calcularValorPremio() {
+        return 0;
+    }
+
+    @Override
+    public String descrever() {
+        return null;
+    }
+}

824c3dd459279f7404a7cfdc791535d35f467b15
Luiz <luizhmartins11@gmail.com>
2023-07-18 13:15:29 -0300

    implementando classe ImovelResidencial

---
+++ src/Cap14/interfaces/seguro/dominio/ImovelResidencial.java
@@ -0,0 +1,39 @@
+package Cap14.interfaces.seguro.dominio;
+
+import Cap14.interfaces.seguro.interfaces.BemSeguravel;
+
+public class ImovelResidencial implements BemSeguravel {
+    private double valorMercado;
+    private int areaConstruida;
+
+    public ImovelResidencial(double valorMercado, int areaConstruida) {
+        this.valorMercado = valorMercado;
+        this.areaConstruida = areaConstruida;
+    }
+
+    public double getValorMercado() {
+        return valorMercado;
+    }
+
+    public void setValorMercado(double valorMercado) {
+        this.valorMercado = valorMercado;
+    }
+
+    public int getAreaConstruida() {
+        return areaConstruida;
+    }
+
+    public void setAreaConstruida(int areaConstruida) {
+        this.areaConstruida = areaConstruida;
+    }
+
+    @Override
+    public double calcularValorPremio() {
+        return 0;
+    }
+
+    @Override
+    public String descrever() {
+        return null;
+    }
+}

ba8104fdc43c40c86b9e1ade03b7c13977b8134c
Luiz <luizhmartins11@gmail.com>
2023-07-19 09:27:32 -0300

    implementando classe Principal

---
+++ src/Cap15/boasPraticas/locacao/test/Principal.java
@@ -0,0 +1,16 @@
+package Cap15.boasPraticas.locacao.test;
+
+import Cap15.boasPraticas.locacao.domain.Locacao;
+import Cap15.boasPraticas.locacao.domain.LocacaoPorHora;
+import Cap15.boasPraticas.locacao.domain.Notebook;
+
+public class Principal {
+    public static void main(String[] args) {
+        Notebook notebook = new Notebook("Macbook Pro i7 16gb", 5, 100);
+
+        Locacao locacao = new LocacaoPorHora(notebook);
+        double valorDevido = locacao.calcularValorDevido(10);
+
+        System.out.printf("Valor a pagar: R$%.2f%n", valorDevido);
+    }
+}

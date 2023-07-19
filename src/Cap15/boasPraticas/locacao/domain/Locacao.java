1ef7115d9d58ce1f024ac16963e0757a5f0a4d8f
Luiz <luizhmartins11@gmail.com>
2023-07-19 10:02:03 -0300

    refatorando classe Locacao

--- src/Cap15/boasPraticas/locacao/domain/Locacao.java
+++ src/Cap15/boasPraticas/locacao/domain/Locacao.java
@@ -1,15 +1,25 @@
 package Cap15.boasPraticas.locacao.domain;
 
-public abstract class Locacao {
+public class Locacao {
     private Notebook notebook;
+    private Precificacao precificacao;
 
-    public Locacao(Notebook notebook) {
+    public Locacao(Notebook notebook, Precificacao precificacao) {
         this.notebook = notebook;
+        this.precificacao = precificacao;
     }
 
     public Notebook getNotebook() {
         return notebook;
     }
 
-    public abstract double calcularValorDevido(int horasUtilizadas);
+    public Precificacao getPrecificacao() {
+        return precificacao;
+    }
+
+    public double calcularValorDevido(int horasUtilizadas){
+        double valorTotal = getPrecificacao().calcularValorTotal(getNotebook(), horasUtilizadas);
+
+        return valorTotal;
+    }
 }

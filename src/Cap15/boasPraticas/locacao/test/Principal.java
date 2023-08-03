71b78c5d2bae9fd9f488e9fb68afe3d2c649a727
Luiz <luizhmartins11@gmail.com>
2023-08-03 14:50:07 -0300

    implementando classe SeguroItau

--- src/Cap15/boasPraticas/locacao/test/Principal.java
+++ src/Cap15/boasPraticas/locacao/test/Principal.java
@@ -6,9 +6,10 @@ public class Principal {
     public static void main(String[] args) {
         Notebook notebook = new Notebook("Macbook Pro i7 16gb", 5, 100);
 
+        Seguro seguro = new SeguroSantander();
         Precificacao precificacao = new PrecificacaoPorDia();
 //
-        Locacao locacao = new Locacao(notebook, precificacao);
+        Locacao locacao = new Locacao(notebook, precificacao, seguro);
 
         double valorDevido = locacao.calcularValorDevido(10);
         System.out.printf("Valor a pagar: R$%.2f%n", valorDevido);

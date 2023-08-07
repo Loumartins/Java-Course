40bcb38c45769d3a7d210e3e239439a0a16c7338
Luiz <luizhmartins11@gmail.com>
2023-08-07 10:14:20 -0300

    Refatorando classe ContaComTributacao

---
+++ src/Cap15/boasPraticas/banco/loubank/ContaComTributacao.java
@@ -0,0 +1,44 @@
+package Cap15.boasPraticas.banco.loubank;
+
+import Cap15.boasPraticas.banco.javabank.Conta;
+
+public class ContaComTributacao implements Conta {
+
+    public static final double TAXA_IMPOSTO_MOVIMENTACAO = 0.1;
+    private Conta contaOriginal;
+
+    public ContaComTributacao(Conta contaOriginal) {
+        this.contaOriginal = contaOriginal;
+    }
+
+
+    @Override
+    public double getSaldo() {
+        return contaOriginal.getSaldo();
+    }
+
+    @Override
+    public void sacar(double valor) {
+        contaOriginal.sacar(valor);
+        debitarImpostoMovimentacao(valor);
+    }
+
+    @Override
+    public void depositar(double valor) {
+        contaOriginal.depositar(valor);
+    }
+
+    @Override
+    public void transferir(Conta conta, double valor) {
+        contaOriginal.transferir(conta, valor);
+    }
+
+    @Override
+    public void aplicarEmInvestimento(double valor) {
+        contaOriginal.aplicarEmInvestimento(valor);
+    }
+
+    private void debitarImpostoMovimentacao(double valor) {
+        contaOriginal.sacar(valor * TAXA_IMPOSTO_MOVIMENTACAO);
+    }
+}

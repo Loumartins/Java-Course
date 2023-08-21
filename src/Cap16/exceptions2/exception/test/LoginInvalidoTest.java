05db78604ce7025022e3ed6cc3c1060ed6aa9860
Luiz <luizhmartins11@gmail.com>
2023-08-21 16:11:43 -0300

    pegando a exception

--- src/Cap16/exceptions2/exception/test/LoginInvalidoTest.java
+++ src/Cap16/exceptions2/exception/test/LoginInvalidoTest.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
 
 public class LoginInvalidoTest {
     public static void main(String[] args) {
-
+        try {
+            logar();
+        } catch (LoginInvalidoException e) {
+            e.printStackTrace();
+        }
     }
     private static void logar() throws LoginInvalidoException {
         Scanner teclado = new Scanner(System.in);

365656e68abf4ff6e3838188557f1cb90b70d52f
Luiz <luizhmartins11@gmail.com>
2023-08-21 16:07:28 -0300

    gerando exception customizada

---
+++ src/Cap16/exceptions2/exception/domain/LoginInvalidoException.java
@@ -0,0 +1,8 @@
+package Cap16.exceptions2.exception.domain;
+
+public class LoginInvalidoException extends Exception{
+
+    public LoginInvalidoException(String message) {
+        super(message);
+    }
+}

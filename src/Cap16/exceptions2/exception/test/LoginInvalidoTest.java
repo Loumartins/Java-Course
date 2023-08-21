824904db56f2a8986fb719d886eaf017631155f1
Luiz <luizhmartins11@gmail.com>
2023-08-21 16:11:02 -0300

    criando metodo privado

---
+++ src/Cap16/exceptions2/exception/test/LoginInvalidoTest.java
@@ -0,0 +1,23 @@
+package Cap16.exceptions2.exception.test;
+
+import Cap16.exceptions2.exception.domain.LoginInvalidoException;
+
+import java.util.Scanner;
+
+public class LoginInvalidoTest {
+    public static void main(String[] args) {
+
+    }
+    private static void logar() throws LoginInvalidoException {
+        Scanner teclado = new Scanner(System.in);
+        String usernameDB = "Luiz";
+        String senhaDB = "2040";
+        System.out.println("Digite o usuário");
+        String usernameUsuario = teclado.nextLine();
+        System.out.println("Digite a senha");
+        String senhaUsuario = teclado.nextLine();
+        if (!usernameDB.equals(usernameUsuario) || !senhaDB.equals(senhaUsuario)){
+            throw new LoginInvalidoException("Usuário ou senha inválidos");
+        }
+     }
+}

d4d3f8aede6a1163ad68d648697937d199559630
Luiz <luizhmartins11@gmail.com>
2023-08-22 09:14:55 -0300

    gerando exception customizada

---
+++ src/Cap16/exceptions/exercicios/domain/ContaInativaException.java
@@ -0,0 +1,8 @@
+package Cap16.exceptions.exercicios.domain;
+
+public class ContaInativaException extends RuntimeException {
+
+    public ContaInativaException(String message) {
+        super(message);
+    }
+}

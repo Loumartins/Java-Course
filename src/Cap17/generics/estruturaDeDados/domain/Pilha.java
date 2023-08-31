13e63ec1d49625453c5143f207d40f09d550c288
Luiz <luizhmartins11@gmail.com>
2023-08-31 16:30:50 -0300

    tornando a classe Pilha em genérica

--- src/Cap17/generics/estruturaDeDados/domain/Pilha.java
+++ src/Cap17/generics/estruturaDeDados/domain/Pilha.java
@@ -2,23 +2,23 @@ package Cap17.generics.estruturaDeDados.domain;
 
 import java.util.Arrays;
 
-public class Pilha {
-    private Object[] itens;
+public class Pilha <T>  {
+    private T[] itens;
 
     public Pilha(){
-        itens = new Object[0];
+        itens = (T[]) new Object[0];
     }
 
-    public void colocar(Object item){
+    public void colocar(T item){
         itens = Arrays.copyOf(itens, itens.length+1);
         itens[itens.length-1] = item;
     }
 
-    public Object retirar(){
+    public T retirar(){
         if (itens.length == 0){
             throw new PilhaVaziaException("Pilha sem itens");
         }
-        Object item = itens[itens.length - 1];
+        T item = itens[itens.length - 1];
         itens = Arrays.copyOf(itens, itens.length-1);
 
         return item;

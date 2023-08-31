230d4fb69c1fcbd82f3576755040aa0ea391049b
Luiz <luizhmartins11@gmail.com>
2023-08-31 16:20:33 -0300

    implementando classe Produto

---
+++ src/Cap17/generics/estruturaDeDados/domain/loja/Produto.java
@@ -0,0 +1,18 @@
+package Cap17.generics.estruturaDeDados.domain.loja;
+
+public class Produto {
+
+    private String descricao;
+
+    public Produto(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}

3908b47106cd23cca5b083bb74e4ae407374ded0
Luiz <luizhmartins11@gmail.com>
2023-08-31 16:17:49 -0300

    implementando exception

---
+++ src/Cap17/generics/estruturaDeDados/domain/PilhaVaziaException.java
@@ -0,0 +1,7 @@
+package Cap17.generics.estruturaDeDados.domain;
+
+public class PilhaVaziaException extends RuntimeException {
+    public PilhaVaziaException(String message) {
+        super(message);
+    }
+}

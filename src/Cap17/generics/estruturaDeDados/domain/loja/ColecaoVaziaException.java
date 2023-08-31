48fd18c31c9f3b25b6b11a907c068f9d214a0d68
Luiz <luizhmartins11@gmail.com>
2023-08-31 16:42:08 -0300

    implementando exception

---
+++ src/Cap17/generics/estruturaDeDados/domain/loja/ColecaoVaziaException.java
@@ -0,0 +1,7 @@
+package Cap17.generics.estruturaDeDados.domain.loja;
+
+public class ColecaoVaziaException extends RuntimeException{
+    public ColecaoVaziaException(String message) {
+        super(message);
+    }
+}

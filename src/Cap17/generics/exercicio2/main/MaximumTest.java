003015be80fa18d9c6b09ab283be06f4fe741431
Luiz <luizhmartins11@gmail.com>
2023-09-04 09:06:10 -0300

    estudos sobre generics

--- src/Cap17/generics/exercicio2/main/MaximumTest.java
+++ src/Cap17/generics/exercicio2/main/MaximumTest.java
@@ -1,7 +1,13 @@
 package Cap17.generics.exercicio2.main;
 
+import Cap17.generics.exercicio2.domain.Maximum;
+
+import java.util.Locale;
+
 public class MaximumTest {
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
+        System.out.println("O valor maximo entre 4,5,6 é: " + Maximum.comparador(4,5,6) + " ");
+        System.out.println("O valor maximo entre 3.5,5.4,12.2 é: " + Maximum.comparador(3.5,5.4,12.2) + " ");
     }
 }

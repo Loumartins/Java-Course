2071cdd3aff8fa687e8ac38ad024b37ffe099028
Luiz <luizhmartins11@gmail.com>
2023-09-05 10:10:04 -0300

    implementando exception

---
+++ src/Cap18/Collections/hotelaria/domain/exceptions/HotelJaExistenteException.java
@@ -0,0 +1,8 @@
+package Cap18.Collections.hotelaria.domain.exceptions;
+
+public class HotelJaExistenteException extends RuntimeException {
+
+    public HotelJaExistenteException(String message) {
+        super(message);
+    }
+}

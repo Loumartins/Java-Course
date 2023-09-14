0b24f4ec3db10c429b213b25da3f151a3e93f3c1
Luiz <luizhmartins11@gmail.com>
2023-09-14 13:12:56 -0300

    gerando exception

---
+++ src/Cap18/Collections/exercicio/agencia/exceptions/PacoteDeViagemJaExistenteException.java
@@ -0,0 +1,8 @@
+package Cap18.Collections.exercicio.agencia.exceptions;
+
+public class PacoteDeViagemJaExistenteException extends RuntimeException {
+
+    public PacoteDeViagemJaExistenteException(String message) {
+        super(message);
+    }
+}

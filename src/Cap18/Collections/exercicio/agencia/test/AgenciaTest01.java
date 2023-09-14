5119daee4886e2f67f2dff17eb056b3288e7167f
Luiz <luizhmartins11@gmail.com>
2023-09-14 14:00:52 -0300

    gerando classe test

---
+++ src/Cap18/Collections/exercicio/agencia/test/AgenciaTest01.java
@@ -0,0 +1,36 @@
+package Cap18.Collections.exercicio.agencia.test;
+
+import Cap18.Collections.exercicio.agencia.domain.CadastroPacoteViagem;
+import Cap18.Collections.exercicio.agencia.domain.PacoteViagem;
+
+import java.util.List;
+
+public class AgenciaTest01 {
+    public static void main(String[] args) {
+        CadastroPacoteViagem cadastroPacoteViagem = new CadastroPacoteViagem();
+        cadastroPacoteViagem.adicionar("Istambul e Capadócia(20 noites)", 18_000);
+        cadastroPacoteViagem.adicionar("Neve em Bariloche (10 noites)", 11_000);
+        cadastroPacoteViagem.adicionar("Disney (10 noites)", 20_000);
+        cadastroPacoteViagem.adicionar("Natal Luz em Gramado (5 noites)", 8_500);
+
+        cadastroPacoteViagem.removerPorDescricao("Disney (10 noites)");
+
+//        cadastroPacoteViagem.ordenar();
+
+//        cadastroPacoteViagem.ordenarPorPreco();
+
+//        PacoteViagem pacoteEncontrado = cadastroPacoteViagem.buscarPorDescricao("Disney (10 noites)");
+//        System.out.println(pacoteEncontrado);
+
+         // imprimirPacotes(cadastroPacoteViagem.obterTodos());
+
+
+
+    }
+
+    private static void imprimirPacotes(List<PacoteViagem> pacotes){
+        for (PacoteViagem pacote : pacotes) {
+            System.out.println(pacote);
+        }
+    }
+}

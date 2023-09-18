f9925debc77ca563ebb3cca792b28fde594a14b3
Luiz <luizhmartins11@gmail.com>
2023-09-18 14:34:22 -0300

    gerando classe Cidade

---
+++ src/Cap18/Collections/mapas/exercicio/domain/Cidade.java
@@ -0,0 +1,65 @@
+package Cap18.Collections.mapas.exercicio.domain;
+
+import java.util.Objects;
+
+public class Cidade {
+    private int codigoIBGE;
+    private String nome;
+    private int totalHabitantes;
+
+    public Cidade(int codigoIBGE, String nome, int totalHabitantes) {
+        Objects.requireNonNull(codigoIBGE);
+        Objects.requireNonNull(nome);
+        this.codigoIBGE = codigoIBGE;
+        this.nome = nome;
+        this.totalHabitantes = totalHabitantes;
+    }
+
+    public int getCodigoIBGE() {
+        return codigoIBGE;
+    }
+
+    public void setCodigoIBGE(int codigoIBGE) {
+        this.codigoIBGE = codigoIBGE;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTotalHabitantes() {
+        return totalHabitantes;
+    }
+
+    public void setTotalHabitantes(int totalHabitantes) {
+        this.totalHabitantes = totalHabitantes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidade{" +
+                "codigoIBGE=" + codigoIBGE +
+                ", nome='" + nome + '\'' +
+                ", totalHabitantes=" + totalHabitantes +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cidade cidade = (Cidade) o;
+
+        return codigoIBGE == cidade.codigoIBGE;
+    }
+
+    @Override
+    public int hashCode() {
+        return codigoIBGE;
+    }
+}

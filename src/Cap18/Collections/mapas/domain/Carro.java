ba3cd85a6aed418766cf61d015ffc3202072e69e
Luiz <luizhmartins11@gmail.com>
2023-09-18 13:17:05 -0300

    gerando classes

---
+++ src/Cap18/Collections/mapas/domain/Carro.java
@@ -0,0 +1,43 @@
+package Cap18.Collections.mapas.domain;
+
+public class Carro {
+    private final String placa;
+    private final String modelo;
+
+    public Carro(String placa, String modelo) {
+        this.placa = placa;
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "placa='" + placa + '\'' +
+                ", modelo='" + modelo + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Carro carro = (Carro) o;
+
+        return placa.equals(carro.placa);
+    }
+
+    @Override
+    public int hashCode() {
+        return placa.hashCode();
+    }
+
+}

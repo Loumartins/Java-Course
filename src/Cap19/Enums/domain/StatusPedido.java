f6903e77e5de5792b8386e71271b6953f3569379
Luiz <luizhmartins11@gmail.com>
2023-09-19 15:50:22 -0300

    refatorando enum

--- src/Cap19/Enums/domain/StatusPedido.java
+++ src/Cap19/Enums/domain/StatusPedido.java
@@ -2,8 +2,24 @@ package Cap19.Enums.domain;
 
 public enum StatusPedido {
     RASCUNHO,
-    EMITIDO,
-    FATURADO,
-    DESPACHADO,
+    EMITIDO(12),
+    FATURADO(10),
+    SEPARADO (8),
+    DESPACHADO(6),
+    ENTREGUE (0),
     CANCELADO;
+
+    private Integer tempoEntregaEmHoras;
+
+    StatusPedido(){
+
+    }
+
+    StatusPedido(Integer tempoEntregaEmHoras) {
+        this.tempoEntregaEmHoras = tempoEntregaEmHoras;
+    }
+
+    public Integer getTempoEntregaEmHoras() {
+        return tempoEntregaEmHoras;
+    }
 }

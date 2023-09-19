ecf7d48e9cb290bc648dea1c91fee824fdc6faf6
Luiz <luizhmartins11@gmail.com>
2023-09-19 10:29:12 -0300

    implementando exception

---
+++ src/Cap18/Collections/encapsulandoColecoes/domain/LimiteDeCompraExcedidoException.java
@@ -0,0 +1,7 @@
+package Cap18.Collections.encapsulandoColecoes.domain;
+
+public class LimiteDeCompraExcedidoException extends RuntimeException{
+    public LimiteDeCompraExcedidoException(String message) {
+        super(message);
+    }
+}

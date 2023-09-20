4445fd11c5b2dd17b2fab783ed3943721083b898
Luiz <luizhmartins11@gmail.com>
2023-09-20 14:48:51 -0300

    implementando enum StatusNotaFiscal

--- src/Cap19/Enums/exercicios/notaFiscal/domain/StatusNotaFiscal.java
+++ src/Cap19/Enums/exercicios/notaFiscal/domain/StatusNotaFiscal.java
@@ -1,7 +1,39 @@
 package Cap19.Enums.exercicios.notaFiscal.domain;
 
 public enum StatusNotaFiscal {
-    NAO_EMITIDA,
-    EMITIDA,
-    CANCELADA;
+    NAO_EMITIDA ("Nao emitida"){
+        @Override
+        public boolean emitir() {
+            return false;
+        }
+    }
+    ,
+    EMITIDA ("Emitida"){
+        @Override
+        public boolean emitir() {
+            return true;
+        }
+    },
+    CANCELADA ("Cancelada"){
+        @Override
+        public boolean emitir() {
+            return false;
+        }
+    };
+
+    private String descricao;
+
+    StatusNotaFiscal(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public abstract boolean emitir();
+
+    public boolean cancelar(double valorPedido){
+        return StatusNotaFiscal.EMITIDA.equals(this) && valorPedido <= 1000 || StatusNotaFiscal.NAO_EMITIDA.equals(this);
+    };
+
+    public String getDescricao() {
+        return descricao;
+    }
 }

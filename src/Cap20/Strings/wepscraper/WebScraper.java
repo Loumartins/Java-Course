43168fa0f87a71e345c8998c43194e51ac5aa52a
Luiz <luizhmartins11@gmail.com>
2023-10-05 09:14:31 -0300

    estudo sobre regular expressions

--- src/Cap20/Strings/wepscraper/WebScraper.java
+++ src/Cap20/Strings/wepscraper/WebScraper.java
@@ -5,9 +5,10 @@ import java.util.regex.Pattern;
 
 public class WebScraper {
     public static void main(String[] args) {
-        String html = "<a href=\"mailto:joao@gmail.com\">\n   joao@gmail.com  </a>\n" +
-                "<a>\n   abc@algaworks.com</a><a>xyz@algaworks.com</a>" +
-                "<strong>maria@algaworks.com</strong>";
+        String html = """
+                <a href="mailto:joao@gmail.com"> joao@gmail.com  </a>
+                <a> abc@algaworks.com</a><a>xyz@algaworks.com</a><strong>maria@algaworks.com</strong>
+                """;
         //System.out.println(html);
 
         String regex = "<.*?>\\s*([\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3})\\s*</.*?>";

e5b367c7c9008ba3c929f43a14ddcb40dc5b56c5
Luiz <luizhmartins11@gmail.com>
2023-10-30 11:56:36 -0300

    estudos sobre period

--- src/Cap23/DateCalendarModern/main/CalculandoPeriodosDeObjetosTemporais.java
+++ src/Cap23/DateCalendarModern/main/CalculandoPeriodosDeObjetosTemporais.java
@@ -2,6 +2,7 @@ package Cap23.DateCalendarModern.main;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class CalculandoPeriodosDeObjetosTemporais {
     public static void main(String[] args) {
@@ -11,5 +12,22 @@ public class CalculandoPeriodosDeObjetosTemporais {
         Period tempoEntrega = Period.between(dataCompra, dataEntrega).normalized();
 
         System.out.println(tempoEntrega);
+
+        //Utilizando unidades temporais
+
+        long TempoDeEntregaEmSemanas = dataCompra.until(dataEntrega, ChronoUnit.DAYS);
+
+        System.out.println(TempoDeEntregaEmSemanas);
+
+        //Utilizando o localDate com o Period
+
+        System.out.println("---------");
+
+        Period tempoMediaEntrega = Period.parse("P1M5D");
+        LocalDate compraRealizada = LocalDate.now();
+
+        LocalDate dataDeEntregaPrevisa = compraRealizada.plus(tempoMediaEntrega);
+
+        System.out.println(dataDeEntregaPrevisa);
     }
 }

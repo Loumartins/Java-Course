82aede820a8134158e6ecee422ebe049bf3b8300
Luiz <luizhmartins11@gmail.com>
2023-12-07 14:36:08 -0300

    implementando classe pedido

---
+++ src/Cap24/ClassesAninhadas/exercicios/domain/Pedido.java
@@ -0,0 +1,56 @@
+package Cap24.ClassesAninhadas.exercicios.domain;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Pedido {
+
+    private final Cliente cliente;
+    private StatusPedido status = StatusPedido.RASCUNHO;
+    BigDecimal valorTotal = BigDecimal.ZERO;
+    private final List<ItemPedido> itens = new ArrayList<>();
+
+    public Pedido(Cliente cliente) {
+        Objects.requireNonNull(cliente);
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public ItemPedido adicionarItem(String descricao, int quantidade, BigDecimal valorUnitario){
+        ItemPedido item = new ItemPedido(this, descricao, valorUnitario, quantidade);
+        itens.add(item);
+        return item;
+    }
+    public void emitir() {
+        checarPedidoParaModificacao();
+        status = StatusPedido.EMITIDO;
+    }
+
+    public void cancelar() {
+        checarPedidoParaModificacao();
+        status = StatusPedido.CANCELADO;
+    }
+
+    void checarPedidoParaModificacao() {
+        if (!StatusPedido.RASCUNHO.equals(status)) {
+            throw new IllegalArgumentException("Pedido não pode ser modificado");
+        }
+    }
+}

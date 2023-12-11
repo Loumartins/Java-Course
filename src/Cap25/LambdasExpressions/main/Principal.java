8e9efc83ede9d22a9aa853b2bc5b913c9392b17c
Luiz <luizhmartins11@gmail.com>
2023-12-11 12:12:12 -0300

    implementando classe Principal

---
+++ src/Cap25/LambdasExpressions/main/Principal.java
@@ -0,0 +1,39 @@
+package Cap25.LambdasExpressions.main;
+
+import Cap25.LambdasExpressions.domain.CadastroCliente;
+import Cap25.LambdasExpressions.domain.Cliente;
+import Cap25.LambdasExpressions.domain.Filtro;
+
+import java.util.List;
+
+public class Principal {
+    public static void main(String[] args) {
+        var cadastroCliente = new CadastroCliente();
+        cadastroCliente.adicionar(new Cliente("Joao", 30));
+        cadastroCliente.adicionar(new Cliente("Maria", 90));
+        cadastroCliente.adicionar(new Cliente("Sebastiao", 50));
+        cadastroCliente.adicionar(new Cliente("Joaquina", 45));
+        cadastroCliente.adicionar(new Cliente("Josefina", 25));
+
+        //gerando classe aninhada
+        Filtro<Cliente> clienteFiltro = new Filtro<Cliente>() {
+            @Override
+            public boolean avaliar(Cliente cliente) {
+                return cliente.getIdade() > 40;
+            }
+        };
+
+        List<Cliente> clienteConsulta = cadastroCliente.consultar(clienteFiltro);
+
+        for (Cliente cliente : clienteConsulta) {
+            System.out.printf("%s - %d%n", cliente.getNome(), cliente.getIdade());
+        }
+
+//        List<Cliente> clienteList = cadastroCliente.getClientes();
+//
+//        for (Cliente cliente : clienteList) {
+//            System.out.printf("%s - %d%n", cliente.getNome(), cliente.getIdade());
+//
+//        }
+    }
+}

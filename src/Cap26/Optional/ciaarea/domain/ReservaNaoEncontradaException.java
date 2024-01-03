5506a77130de01a38a9052024a9b13f41bba2745
Luiz <luizhmartins11@gmail.com>
2024-01-03 08:48:59 -0300

    implementando exception

---
+++ src/Cap26/Optional/ciaarea/domain/ReservaNaoEncontradaException.java
@@ -0,0 +1,8 @@
+package Cap26.Optional.ciaarea.domain;
+
+public class ReservaNaoEncontradaException extends RuntimeException {
+
+    public ReservaNaoEncontradaException(String message) {
+        super(message);
+    }
+}

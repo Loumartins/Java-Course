61d906a03ba184803cff25a51c121fe2cca9b93c
Luiz <luizhmartins11@gmail.com>
2024-02-15 11:30:41 -0300

    implementando o metodo collect()

--- src/cap27/streams/estoque/test/EstoqueTest01.java
+++ src/cap27/streams/estoque/test/EstoqueTest01.java
@@ -2,60 +2,26 @@ package cap27.streams.estoque.test;
 
 import cap27.streams.estoque.domain.CadastroProduto;
 import cap27.streams.estoque.domain.Categoria;
-import cap27.streams.estoque.domain.Fabricante;
 import cap27.streams.estoque.domain.Produto;
 
-import java.math.BigDecimal;
+
 import java.util.*;
-import java.util.function.IntBinaryOperator;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 
 public class EstoqueTest01 {
     public static void main(String[] args) {
         var cadastroDeProdutos = new CadastroProduto();
         List<Produto> produtos = cadastroDeProdutos.obterTodos();
 
-//        operacoes de reduçoes especiais
+//      coletando elementos do Stream em lista
 
-        int totalEstoque = produtos.stream()
-                .mapToInt(Produto::getQuantidade)
-                .sum();
-
-        System.out.println(totalEstoque);
-
-        double mediaEstoque = produtos.stream()
-                .mapToInt(Produto::getQuantidade)
-                .average()
-                .orElseThrow(() -> new RuntimeException("Valores não encontrados"));
-
-        System.out.println(mediaEstoque);
-
-        long totalProdutosComEstoque = produtos.stream()
+       List<Categoria> categorias = produtos.stream()
                 .filter(Produto::temEstoque)
-                .count();
+                .flatMap(produto-> produto.getCategorias().stream())
+                .distinct()
+                .collect(Collectors.toList());
 
-        System.out.println(totalProdutosComEstoque);
-
-        System.out.println("------------------------------");
-//        operaçoes de redução min - max
-//        buscando a menor quantidade de produto em estoque
-        int menorQuantidadeEmEstoque = produtos.stream()
-                .filter(Produto::temEstoque)
-                .mapToInt(Produto::getQuantidade)
-                .min()
-                .orElseThrow(() -> new RuntimeException("Menor quantidade nao " +
-                        "encontrado"));
-        System.out.println(menorQuantidadeEmEstoque);
-
-        System.out.println("-------------------------");
-//      buscando o produto mais barato que tem em estoque
-
-        Produto produtoMaisBarato = produtos.stream()
-                .filter(Produto::temEstoque)
-                .min(Comparator.comparing(Produto::getPreco))
-                .orElseThrow(() -> new RuntimeException("Produto mais barato nao " +
-                        "encontrado"));
-
-        System.out.println(produtoMaisBarato);
+        System.out.println(categorias);
     }
 }
